d9ed71e26a5cf02080238e22d8871ac1c126d453
tophat8855 <hrfarley@gmail.com>
2015-02-17 15:41:50 -0800

    messages for save and delete

--- app/controllers/notes.js
+++ app/controllers/notes.js
@@ -1,21 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  message: "Spill the beans, jimmy.",
   actions: {
     createNote: function() {
       var content = this.get('newNote');
       var note = this.store.createRecord('note', {content: content,});
       note.save();
       this.set('newNote', '');
+      this.set('message', "Saved!");
     },
     deleteNote: function(note) {
+      var _this = this;
       this.store.find('note', note.id).then(function(note) {
-        note.deleteRecord();
-        note.get('isDeleted');
-        note.save();
-
-        // OR post.destroyRecord();
+        note.destroyRecord().then(function() {
+          _this.set('message', 'DELETED');
+        });
       });
+
     },
   }
 });
